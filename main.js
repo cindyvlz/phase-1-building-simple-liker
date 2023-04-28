51eefcc4190dae87f1eaf20f6d55218380644f04
Cindy Velasquez <ciindvlz@hotmail.com>
2023-04-28 19:22:25 -0400

    Liker

--- main.js
+++ main.js
@@ -3,9 +3,23 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+let heartGlyph = document.querySelector(".like").children[0];
+heartGlyph.addEventListener("click", () => {
+  if (heartGlyph.className === "like-glyph") {
+    mimicServerCall()
+      .then(() => {
+        heartGlyph.setAttribute("class", "activated-heart");
+      })
+      .catch(() => {
+        document.querySelector("#modal").setAttribute("class", "");
+        setTimeout(() => {
+          document.querySelector("#modal").setAttribute("class", "hidden");
+        }, 3000);
+      });
+  } else {
+    heartGlyph.setAttribute("class", "like-glyph");
+  }
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
